459bda305fbbbef8ba14d7104dc95a44b65a8603
Adlaine <adlainet@gmail.com>
2021-10-05 15:40:22 -0300

    novos teste e refatoracao

--- src/main/java/cursoAppium/FormularioTeste.java
+++ src/main/java/cursoAppium/FormularioTeste.java
@@ -1,45 +1,87 @@
 package cursoAppium;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import static org.junit.Assert.assertEquals;
 
+import java.net.MalformedURLException;
+
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
+import Appium.core.DSL;
+import Appium.core.DriverFactory;
 import io.appium.java_client.MobileBy;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
+
 
 
 public class FormularioTeste {
+	private DSL dsl = new DSL();
+	
+	@Before
+	public void InicializarAppium() throws MalformedURLException {
+		// Selecionar formulário
+		dsl.clicarPorTexto("Formulário");
+	}
 
 	@Test
 	public void devePreencherCampoTexto() throws MalformedURLException {
-		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-		desiredCapabilities.setCapability("platformName", "Android");
-		desiredCapabilities.setCapability("deviceName", "emulator-5554");
-		desiredCapabilities.setCapability("automationName", "uiautomator2");
-		
-		desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Cursos\\cursoAppium\\src\\main\\resources\\CTAppium_1_2.apk");
-
-		AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-		//Selecionar formulário
-		List<MobileElement> elementosEncontrado = driver.findElements(By.className("android.widget.TextView"));
-
-		elementosEncontrado.get(1).click();
 		// escrever nome
-		MobileElement campoName = driver.findElement(MobileBy.AccessibilityId("nome"));
-		campoName.sendKeys("Adlaine");
+		dsl.escrever(MobileBy.AccessibilityId("nome"), "Adlaine");
+		
 		//checar nome
-		String text = campoName.getText();
-		Assert.assertEquals("Adlaine", text);
-		driver.quit();
+		assertEquals("Adlaine", dsl.obterTexto(MobileBy.AccessibilityId("nome")));
 	}
+
+	@Test
+	public void deveInteragirComCombo() throws MalformedURLException {
+		// clicar no combo
+		dsl.selecionarCombo(MobileBy.AccessibilityId("console"), "XBox One");
+		
+		// verificar a opcao selecionada
+		
+		String texto = dsl.obterTexto(By.xpath("//android.widget.Spinner/android.widget.TextView"));
+		Assert.assertEquals("XBox One", texto);
+	}
+	
+	@Test
+	public void deveInteragirSwitchCheckbox() throws MalformedURLException {
+		//Verificar status dos elementos
+		Assert.assertFalse(dsl.isCheckMarcado(By.className("android.widget.CheckBox")));
+		Assert.assertTrue(dsl.isCheckMarcado(MobileBy.AccessibilityId("switch")));
+		
+		//clicar nos elementos
+		dsl.clicar(By.className("android.widget.CheckBox"));
+		dsl.clicar(MobileBy.AccessibilityId("switch"));
+		
+		//verificar status alterado
+		Assert.assertTrue(dsl.isCheckMarcado(By.className("android.widget.CheckBox")));
+		Assert.assertFalse(dsl.isCheckMarcado(MobileBy.AccessibilityId("switch")));
+	}
+	
+	@Test
+	public void deveRealizarCadastro() throws MalformedURLException {
+		// preencher campos
+		dsl.escrever(By.className("android.widget.EditText"), "Adlaine");
+		dsl.clicar(By.className("android.widget.CheckBox"));
+		dsl.clicar(By.className("android.widget.Switch"));
+		dsl.selecionarCombo(By.className("android.widget.Spinner"), "XBox One");
+
+		
+		// clicar salvar
+		dsl.clicarPorTexto("SALVAR");
+		
+		// validar campos
+		Assert.assertEquals("Nome: Adlaine", dsl.obterTexto((By.xpath("//android.widget.TextView[@text='Nome: Adlaine']"))));
+		Assert.assertEquals("Console: xone", dsl.obterTexto((By.xpath("//android.widget.TextView[starts-with(@text, 'Console:')]"))));
+		Assert.assertTrue(dsl.obterTexto(By.xpath("//android.widget.TextView[starts-with(@text, 'Switch:')]")).endsWith("Off"));
+		Assert.assertTrue(dsl.obterTexto(By.xpath("//android.widget.TextView[starts-with(@text, 'Checkbox:')]")).endsWith("Marcado"));
+		
+	}
+	@After
+	public void tearDown() {
+		DriverFactory.killDriver();
+	}
+	
 }
